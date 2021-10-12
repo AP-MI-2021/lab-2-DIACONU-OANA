30176d6a838799d908077f2a15586d0ef5feadd2
Diaconu Oana <oana.diaconu1@stud.ubbcluj.ro>
2021-10-13 00:25:15 +0300

    Problema superprime

--- main/main.py
+++ main/main.py
@@ -23,9 +23,28 @@ def test_is_palindrome():
 test_is_palindrome()
 
 
+def is_superprime(n) -> bool:
+#Determină dacă un număr este superprim: dacă toate prefixele sale sunt prime.
+    ok=1
+    while n>0 and ok==1:
+        nrd=0
+        for i in range(1,n+1):
+            if n % i == 0:
+                nrd=nrd+1
+        if nrd>2:
+            ok=0
+        n=n//10
+    if ok==1:
+        return True
+    else:
+        return False
 
 
+def test_is_superprime():
+    assert is_superprime(557) == False
+    assert is_superprime(124) == False
+    assert is_superprime(293) == True
+    assert is_superprime(317) == True
 
 
-
-
+test_is_superprime()
